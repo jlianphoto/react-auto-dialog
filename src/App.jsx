363c7841d257db5eaf9682a30b15ad065ef1a024
jlian <872821547@qq.com>
2017-07-14 19:50:52 +0800

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import Wechat from './wechat/wechat.jsx';
+
+
+
+let dialog = [
+	{'me' : '你去吃屎'},
+	{'me' : {
+		type : 'img',
+		source : ['http://img4.imgtn.bdimg.com/it/u=1365112016,127878692&fm=26&gp=0.jpg']
+	}},
+	{'me' : {
+		type : 'video',
+		cover : 'http://img4.imgtn.bdimg.com/it/u=1365112016,127878692&fm=26&gp=0.jpg',
+		source : 'asdfadfasd'
+	}},
+	{'me' : {
+		type : 'question',
+		msg : '你是不是傻',
+	}},
+	{'德善' : {
+		type: 'answer',
+		key : '是'
+	}},
+	{'me' : '我知道了 , 你是傻'},
+	{'me' : '知道了, 你不是傻'},
+	// {'德善' : '垃圾,垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾垃圾'},
+	// {'me' : '你去吃屎'},
+	// {'德善' : '垃圾'},
+	// {'me' : '你去吃屎'},
+	// {'德善' : '垃圾'},
+	// {'me' : '你去吃屎'},
+	// {'德善' : '垃圾'},
+	// {'me' : '你去吃屎'},
+	// {'德善' : '垃圾'},
+	// {'me' : '你去吃屎'},
+	// {'德善' : '垃圾'},
+	// {'me' : '你去吃屎'},
+	// {'德善' : '垃圾'},
+	// {'me' : '你去吃屎'},
+	// {'德善' : '垃圾'},
+	// {'me' : '你去吃屎'},
+	// {'德善' : '垃圾'},
+	// {'me' : '你去吃屎'},
+	// {'德善' : '垃圾'},
+	// {'me' : '你去吃屎'},
+	// {'德善' : '垃圾'}
+]
+
+
+
+let config = {
+	speed: 1000,
+	mine: {
+		name : '狗焕',
+		img : require('./img/huan.jpg')
+	},
+	orthers:[
+		{
+			name : '德善',
+			img : require('./img/de.jpg')
+		}
+	],
+	fuzzy : {
+		key : [
+			{
+				key : 'laji',
+				msg :  '傻B'
+			}
+		],
+		default : '再见'
+	}
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Wechat dialog={dialog} config={config}></Wechat>
+    );
+  }
+}
+
+export default App;
---
+++ src/wechat/wechat.jsx
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import './wechat.scss';
+
+class Wechat extends Component {
+  constructor(props) {
+      super(props);
+      this.dialogs = props.dialog;
+      this.index = 1;
+
+      this.state = {
+        dialogs : [this.dialogs[0]]
+      };
+
+      this.timer = null;
+      this.add = this.add.bind(this);
+  }
+
+
+  add(){
+    let aaa = {
+      '德善' : '粉肠粉肠粉肠粉肠粉肠粉肠'
+    }
+    this.state.dialogs.push(aaa);
+    this.setState({
+      data : this.state.data
+    })
+  }
+
+
+
+  componentDidMount(){
+    console.log(this.dialogs)
+    console.log(this.props.config)
+    //timer
+    // this.$view = document.querySelector('#hiddenView');
+    // this.timer = setInterval(()=>{
+    //   if (this.index >= this.dialogs.length) {
+    //     clearInterval(this.timer);
+    //     return
+    //   }
+    //   this.state.dialogs.push(this.dialogs[this.index])
+    //   this.setState({
+    //     dialogs : this.state.dialogs
+    //   })
+    //   this.$view.scrollIntoView()
+    //   this.index++;
+    // },this.props.data.speed)
+  }
+
+
+  render() {
+
+    // let list = this.state.dialogs.map((item,index)=>{
+    //   let name = Object.keys(item)[0],
+    //       content = item[name],
+    //       img = null,
+    //       type = '';
+
+    //   if (name==='me') {
+    //     name = this.props.data.mine.name;
+    //     img = this.props.data.mine.img;
+    //     type = 'mine'
+    //   }else{
+    //     img = this.props.data.orthers.find(obj=>obj.name === name).img;
+    //     type = 'others';
+    //   }
+
+
+    //     return  <li className={type} key={index}>
+    //               <img src={img} alt=""/>
+    //               <div className="content">
+    //                 <p>{name}</p>
+    //                 <div className='bubble'>
+    //                   {content}
+    //                 </div>
+    //               </div>
+    //             </li>
+    // })
+
+
+
+
+
+    return (
+      <div>
+        <button onClick={this.add}>试试</button>
+        <ul className = 'wechat'>
+          <li  className="mine" >
+           <img src={require('../img/huan.jpg')} className='portrait' alt=""/>
+           <div className="content">
+             <p>sdf</p>
+             <div className='bubble'>
+               asfadfasdfa
+             </div>
+           </div>
+         </li>
+          <li className="others">
+            <img src={require('../img/huan.jpg')} className='portrait' alt=''/>
+            <div className='content'>
+              <p>我啊</p>
+              <img src='http://img4.imgtn.bdimg.com/it/u=1365112016,127878692&fm=26&gp=0.jpg' alt='' className='type-img'/>
+            </div>
+          </li>
+        </ul>
+        <div id="hiddenView"></div>
+      </div>
+    );
+  }
+}
+
+export default Wechat;
