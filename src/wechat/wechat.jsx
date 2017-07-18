edf535288aa2ece37d75a376b2c52152a5b46dc0
jlian <872821547@qq.com>
2017-07-18 15:49:57 +0800

    timer

--- src/wechat/wechat.jsx
+++ src/wechat/wechat.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import createReactClass from 'create-react-class';
 import './wechat.scss';
 
 class Wechat extends Component {
@@ -12,12 +11,17 @@ class Wechat extends Component {
         dialogs : [this.dialogs[0]],
         isShow : false,
         writable : true,
-        isSend : false
+        isSend : false,
+        iframeSource:'https://v.qq.com/iframe/player.html?vid=m0357eb6ia2&tiny=0&auto=0',
       };
 
       this.timer = null;
   }
 
+
+  showImg = () =>{
+    console.log(1111)
+  }
 
   closeRadio = ()=>{
     this.setState({
@@ -25,8 +29,9 @@ class Wechat extends Component {
     })
   }
 
-  showRadio = ()=>{
+  showRadio = (source)=>{
     this.setState({
+      iframeSource : source,
       isShow : true
     })
   }
@@ -47,90 +52,105 @@ class Wechat extends Component {
 
 
 
-  // componentDidMount(){
-  //   console.log(this.dialogs)
-  //   console.log(this.props.config)
-  //   //timer
-  //   this.$view = document.querySelector('#hiddenView');
-  //   this.timer = setInterval(()=>{
-  //     if (this.index >= this.dialogs.length) {
-  //       clearInterval(this.timer);
-  //       return
-  //     }
-  //     this.state.dialogs.push(this.dialogs[this.index])
-  //     this.setState({
-  //       dialogs : this.state.dialogs
-  //     })
-  //     this.$view.scrollIntoView()
-  //     this.index++;
-  //   },this.props.config.speed)
-  // }
+
+  componentDidMount(){
+    //timer
+    this.$view = document.querySelector('#hiddenView');
+    this.timer = setInterval(()=>{
+      if (this.index >= this.dialogs.length) {
+        clearInterval(this.timer);
+        return
+      }
+      this.state.dialogs.push(this.dialogs[this.index])
+      this.setState({
+        dialogs : this.state.dialogs
+      })
+      this.$view.scrollIntoView()
+      this.index++;
+    },5000)
+  }
+
+
+  listHandler = ()=>{
+      let list = this.state.dialogs.map((item,index)=>{
+        let name = Object.keys(item)[0],
+            content = item[name],
+            img = null,
+            who = '';
+
+        if (name==='me') {
+          name = this.props.config.mine.name;
+          img = this.props.config.mine.img;
+          who = 'mine'
+        }else{
+          img = this.props.config.orthers.find(obj=>obj.name === name).img;
+          who = 'others';
+        }
+
+
+        let type = '';
+        if (typeof content === 'string') {
+          type = 'txt';
+        }else{
+          type = content.type;
+        }
+
+
+        let diffElement = null;
+        switch(type){
+          case 'txt':
+            diffElement = <div className='bubble'>
+                            {content}
+                          </div>
+            break;
+
+          case 'img':
+            var source = content.source;
+            diffElement = <img src={source} onClick={this.showImg} alt='' className='type-img'/>
+            break;
+
+          case 'video':
+            var cover = content.cover;
+            var source = content.source;
+            diffElement = <span className="radio" onClick={(source) =>{this.showRadio(source)}}>
+                            <i className="icon-play"></i>
+                            <img  src={cover} alt='' className='type-img'/>
+                          </span>
+            break
+            
+          case 'question':
+            var question = content.msg;
+            diffElement = <div className='bubble'>
+                            {question}
+                          </div>
+            break
+        }
+
+
+        return  <li className={who} key={index}>
+                    <img src={img} alt="" className='portrait'/>
+                    <div className="content">
+                      <p>{name}</p>
+                      {diffElement}
+                    </div>
+                  </li>
+      })
+
+      return list;
+
+  }
 
 
   render() {
 
-    // let list = this.state.dialogs.map((item,index)=>{
-    //   let name = Object.keys(item)[0],
-    //       content = item[name],
-    //       img = null,
-    //       who = '';
 
-    //   if (name==='me') {
-    //     name = this.props.config.mine.name;
-    //     img = this.props.config.mine.img;
-    //     who = 'mine'
-    //   }else{
-    //     img = this.props.config.orthers.find(obj=>obj.name === name).img;
-    //     who = 'others';
-    //   }
-
-
-    //   let type = '';
-    //   if (typeof content === 'string') {
-    //     type = 'txt';
-    //   }else{
-    //     type = content.type;
-    //   }
-
-
-    //   switch(type){
-    //     case 'txt':
-          
-    //       break;
-    //     case 'img':
-          
-    //       break;
-    //     case 'video':
-
-    //       break
-    //     case 'question':
-
-    //       break
-    //     case 'answer':
-
-    //       break
-    //   }
-
-      
-
-    //     return  <li className={who} key={index}>
-    //               <img src={img} alt=""/>
-    //               <div className="content">
-    //                 <p>{name}</p>
-    //                 <div className='bubble'>
-    //                   {content}
-    //                 </div>
-    //               </div>
-    //             </li>
-    // })
-
-
-
+    let list = this.listHandler();
 
 
     return (
       <div>
         <ul className = 'wechat'>
+          {/*
           <li  className="mine" >
            <img src={require('../img/huan.jpg')} className='portrait' alt=""/>
            <div className="content">
@@ -157,10 +177,13 @@ class Wechat extends Component {
               </span>
             </div>
           </li>
+          */} 
+
+          {list}
         </ul>
         <div className={this.state.isShow?'radio-wrapper':'hide radio-wrapper'}>
           <i className='icon-close' onClick={this.closeRadio}></i>
-          {this.state.isShow && <iframe allowfullscreen="true" src="https://v.qq.com/iframe/player.html?vid=m0357eb6ia2&tiny=0&auto=0"></iframe>}
+          {this.state.isShow && <iframe allowfullscreen="true" title="video" src={this.state.iframeSource}></iframe>}
         </div>
         
 
